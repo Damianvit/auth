1769dc486a9e03eeb194b813460033f0ff7af6b1
Damianvit <damianvitchib@gmail.com>
2024-05-05 09:02:43 +0100

    About to start database integration

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { useState, useTransition } from "react";
 import * as z from "zod";
-import { login } from "@/actions/login";
+import { register } from "@/actions/register";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { LoginSchema } from "@/schemas";
+import { RegisterSchema } from "@/schemas";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { FormError } from "@/components/form-error";
@@ -18,23 +18,24 @@ import {
     FormMessage,
 } from "@/components/ui/form";
 import { CardWrapper } from "@/components/auth/card-wrapper";
-export const LoginForm = () => {
+export const RegisterForm = () => {
     const [ispending, startTransition] = useTransition();
     const [error, setError] = useState<string | undefined>("");
     const [success, setSuccess] = useState<string | undefined>("");
-    const form = useForm<z.infer<typeof LoginSchema>>({
-        resolver: zodResolver(LoginSchema),
+    const form = useForm<z.infer<typeof RegisterSchema>>({
+        resolver: zodResolver(RegisterSchema),
         defaultValues: {
             email: "",
             password: "",
+            name: "",
         },
     });
 
-    const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
         setError("");
         setSuccess("");
         startTransition(() => {
-            login(values).then((data) => {
+            register(values).then((data) => {
                 setError(data.error);
                 setSuccess(data.success);
             });
@@ -42,9 +43,9 @@ export const LoginForm = () => {
     };
     return (
         <CardWrapper
-            headerLabel="Welcome back"
-            backButtonLabel="Don't have an account"
-            backButtonHref="/auth/register"
+            headerLabel="Create an account"
+            backButtonLabel="Already have an account"
+            backButtonHref="/auth/login"
             showSocial
         >
             <Form {...form}>
@@ -89,6 +90,23 @@ export const LoginForm = () => {
                                 </FormItem>
                             )}
                         />
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            disabled={ispending}
+                                            placeholder="vitus damain"
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
                     </div>
                     <FormError message={error} />
                     <FormSuccess message={success} />
@@ -97,7 +115,7 @@ export const LoginForm = () => {
                         className="w-full"
                         disabled={ispending}
                     >
-                        Login
+                        Create an account
                     </Button>
                 </form>
             </Form>
